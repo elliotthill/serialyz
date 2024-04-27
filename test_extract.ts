311fb34bb605ad3b61cc73c20334d119442331a2
Elliott Hill <elliott.hill@gmail.com>
2024-04-27 15:53:35 +0100

    Migrate to Bun runtime

---
+++ test_extract.ts
@@ -0,0 +1,3 @@
+export const testExtract = () => {
+    return Promise.resolve("Hello World!");
+}
---
+++ tests/parser.test.ts
@@ -0,0 +1,9 @@
+import { Parser } from "../parser/parser.js";
+import {describe, expect, test} from "bun:test";
+
+describe("testing ", () => {
+
+    test("1+1 = 2", () => {
+        expect(1+1).toBe(3);
+    });
+});
