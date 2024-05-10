44a4ad76ab35647ea7bc87884f963bfcbc09b5a0
Elliott Hill <elliott.hill@gmail.com>
2024-05-10 18:28:00 +0100

    Remove accidentaly console.log

--- tests/field-data/wwwredditcom/reddit.test.ts
+++ tests/field-data/wwwredditcom/reddit.test.ts
@@ -1,19 +1,15 @@
-import {Parser} from "../../../parser/parser.js";
-import {describe, expect, test} from "bun:test";
-import { TextTree } from "../../../parser/types.js";
-import redditcomFieldData from "./wwwredditcom.json" assert {type:"json"};
+import {Parser} from "../../../parser/parser.js"
+import {describe, expect, test} from "bun:test"
+import {TextTree} from "../../../parser/types.js"
+import redditcomFieldData from "./wwwredditcom.json" assert {type: "json"}
 
 describe("reddit.com field test", () => {
-
     test("Parse homepage", () => {
-        const parser = new Parser(redditcomFieldData as TextTree);
-        const output = parser.parse();
+        const parser = new Parser(redditcomFieldData as TextTree)
+        const output = parser.parse()
 
-        expect(output.length).toBe(35);
-        expect(output[0].title).toBe("Murray's game-winner");
-        expect(output[6].title).toStartWith("What's something you think is totally");
-        //expect(output[34].title).toBe("Promoted");
-        console.log(output);
-    });
-
-});
+        expect(output.length).toBe(35)
+        expect(output[0].title).toBe("Murray's game-winner")
+        expect(output[6].title).toStartWith("What's something you think is totally")
+    })
+})
